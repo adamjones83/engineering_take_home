24c9f4d4345449a049060260deb5c62b3504b4e5
Adam Jones <adamjones83@gmail.com>
2024-11-29 18:21:20 -0600

    update schema from running updated migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_27_021752) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_29_143022) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "building_custom_fields", force: :cascade do |t|
-    t.bigint "client_id", null: false
+    t.bigint "custom_field_id", null: false
     t.bigint "building_id", null: false
     t.string "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["building_id"], name: "index_building_custom_fields_on_building_id"
-    t.index ["client_id"], name: "index_building_custom_fields_on_client_id"
+    t.index ["custom_field_id"], name: "index_building_custom_fields_on_custom_field_id"
   end
 
   create_table "buildings", force: :cascade do |t|
@@ -40,17 +40,26 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_27_021752) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "custom_field_choices", force: :cascade do |t|
+    t.bigint "custom_field_id", null: false
+    t.string "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["custom_field_id"], name: "index_custom_field_choices_on_custom_field_id"
+  end
+
   create_table "custom_fields", force: :cascade do |t|
     t.bigint "client_id", null: false
     t.string "name"
-    t.string "type"
+    t.string "field_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["client_id"], name: "index_custom_fields_on_client_id"
   end
 
   add_foreign_key "building_custom_fields", "buildings"
-  add_foreign_key "building_custom_fields", "clients"
+  add_foreign_key "building_custom_fields", "custom_fields"
   add_foreign_key "buildings", "clients"
+  add_foreign_key "custom_field_choices", "custom_fields"
   add_foreign_key "custom_fields", "clients"
 end
